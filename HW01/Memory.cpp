4a6ffb7744472018e45a8b0ed6822fea8b91aa97
kasimsuzen <kasmszen@gmail.com>
2016-03-07 22:52:00 +0200

    Memory class implemented

---
+++ HW01/Memory.cpp
@@ -0,0 +1,14 @@
+//
+// Created by siyah on 07.03.2016.
+//
+
+#include "Memory.h"
+
+Memory::Memory(int indexData, int valueData){
+    index = indexData;
+    value = valueData;
+}
+
+Memory::~Memory() {
+
+}
---
+++ HW01/Memory.h
@@ -0,0 +1,21 @@
+//
+// Created by siyah on 07.03.2016.
+//
+
+#ifndef HW01_MEMORY_H
+#define HW01_MEMORY_H
+
+
+class Memory {
+public:
+    Memory(int indexData, int valueData);
+
+    ~Memory()();
+
+private:
+    int index;
+    int value;
+};
+
+
+#endif //HW01_MEMORY_H
