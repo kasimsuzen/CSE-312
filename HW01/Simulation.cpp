f6fbbc7bda8558c06283fe8d2a9b6f445d5cdf3b
kasimsuzen <kasmszen@gmail.com>
2016-03-09 23:24:43 +0200

    parseFile function implementation finished Instructions individual functions implemented

--- HW01/Simulation.cpp
+++ HW01/Simulation.cpp
@@ -5,11 +5,14 @@
 #include <fstream>
 #include <streambuf>
 #include <sstream>
+#include <cmath>
 #include "Simulation.h"
 #include "Instruction.h"
 #include "Memory.h"
 
 Simulation::Simulation(string &filename, int md) {
+    memory.clear();
+    instructions.clear();
     parseFile(filename);
     mode=md;
 }
@@ -31,7 +34,7 @@ void Simulation::printMemory() {
 
 void Simulation::parseFile(string &fileName) {
     string fileContent;
-    int i,j;
+    int i,j,dataStart,dataEnd,instructionStart,instructionEnd,index=0;
     ifstream fileStream;
     fileStream.open(fileName.c_str());
     stringstream strStream;
@@ -50,8 +53,13 @@ void Simulation::parseFile(string &fileName) {
     strStream.str("");
     strStream.clear();
 
-    cout << fileContent;
+    // To erase 3 junk character
+    fileContent.erase(0,3);
 
+    #ifdef DEBUG
+        cout << "Raw file output" << endl;
+        cout << fileContent << endl;
+    #endif
     // To uncomment assembly file
     for (i = 0; i < fileContent.size(); ++i) {
         if(fileContent[i] == '\n' && i < fileContent.size()-1 && fileContent[i+1] == '\n') {
@@ -70,8 +78,196 @@ void Simulation::parseFile(string &fileName) {
         }
     }
 
-    cout << "after un comments" << endl;
-    cout << fileContent;
+    #ifdef DEBUG
+        cout << "after un comments" << endl;
+        cout << fileContent;
+    #endif
+
+    dataStart = (int) fileContent.find(DATA_START);
+    dataEnd = (int) fileContent.find(DATA_END);
+    instructionStart = (int) fileContent.find(INSTR_START);
+    instructionEnd = (int) fileContent.find(INSTR_END);
+    if (dataStart == string::npos || dataEnd == string::npos || instructionStart == string::npos || instructionEnd  == string::npos){
+        cerr << "This has not data or instruction sections \n";
+    }
+
+    string temp = DATA_START;
+    Memory memTemp;
+    int spaceToJump=0;
+
+    for (int k = (int) (temp.length() + 3); k < dataEnd; ) {
+        spaceToJump = 0;
+        j=k;
+        while(fileContent[j] != '\n')
+            ++j;
+
+        index = atoi(&fileContent.c_str()[k]);
+        if(index != 0)
+            spaceToJump = (int) log10((double)(index));
+
+        memTemp = Memory(index,atoi(&fileContent.c_str()[k + spaceToJump +2]));
+        #ifdef DEBUG
+            cout << k << " " << fileContent[k] << " " << index << " " << memTemp.getIndex() << " " << memTemp.getValue() << endl;
+        #endif
+
+        memory.push_back(memTemp);
+        k=j+1;
+
+    }
+
+    #ifdef DEBUG
+        cout << "memtest" << endl;
+        printMemory();
+    #endif
+
+    temp = INSTR_START;
+    int firstOperand,secondOperand,secondSpace;
+    string operand;
+    char tt;
+    for (int l = instructionStart + (int) (temp.length() + 2); l < fileContent.size(); ) {
+        operand.clear();
+        spaceToJump = 0;
+        secondSpace = 0;
+        index = atoi(&fileContent.c_str()[l]);
+        if(index != 0)
+            spaceToJump = (int) log10(index);
+
+        j=l;
+        while(fileContent[j] != '\n')
+            ++j;
+        i=l+spaceToJump+2;
+        while(fileContent[i] != ' '){
+            tt = fileContent[i];
+            operand.push_back(tt);
+            ++i;
+        }
+        temp = fileContent.substr(l,j-l);
+
+        if (temp.find("SET") != string::npos){
+            temp = "SET";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace+1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("CPY") != string::npos){
+            temp = "CPY";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("CPYI") != string::npos ){
+            temp = "CPYI";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("CPYI2") != string::npos){
+            temp = "CPYI2";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("ADD") != string::npos){
+            temp = "ADD";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("ADDI") != string::npos){
+            temp = "ADDI";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("SUBI") != string::npos){
+            temp = "SUBI";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if (temp.find("JIF") != string::npos){
+            temp = "JIF";
+            firstOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3]);
+            if (firstOperand != 0) {
+                secondSpace = (int) log10(abs(firstOperand));
+            }
+            if(firstOperand < 0)
+                ++secondSpace;
+            secondOperand = atoi(&fileContent.c_str()[l + temp.length() + spaceToJump + 3 + secondSpace + 1]);
+            instructions.push_back(Instruction(index,temp,firstOperand,secondOperand));
+        }
+
+        else if(temp.find("HLT") != string::npos){
+            temp = "HLT";
+            instructions.push_back(Instruction(index,temp));
+        }
+        else if (temp.find("SYS PARAMS") != string::npos){
+            temp = "SYS PARAMS";
+            instructions.push_back(Instruction(index,temp));
+        }
+        else{
+            cerr << "Unknown instruction "<< endl;
+            exit(-1);
+        }
+
+        ++j;
+        l=j;
+        if(l == instructionEnd)
+            break;
+    }
+
+#ifdef DEBUG
+    cout << "Instruction list" << endl;
+    printInstructionList();
+#endif
 
 }
 
@@ -85,4 +281,96 @@ Memory &Simulation::getMemory(int index) {
 
 Instruction &Simulation::getInstruction(int index) {
     return instructions[index];
+}
+
+void Simulation::printInstructionList() {
+    for (int i = 0; i < instructions.size(); ++i) {
+        cout << instructions[i].getIndex() << " " << instructions[i].getInstruction() << " " << instructions[i].getFirstOperand() << " " << instructions[i].getSecondOperand() << endl;
+    }
+}
+
+bool Simulation::funcSET(const Instruction &inst) {
+    if(memory.size() <= inst.getSecondOperand())
+        return false;
+    else{
+        memory[inst.getSecondOperand()].setValue(inst.getFirstOperand());
+        return true;
+    }
+}
+
+bool Simulation::funcCPY(const Instruction &inst) {
+    if(inst.getFirstOperand() > memory.size() || inst.getSecondOperand() > memory.size())
+        return false;
+    else{
+        memory[inst.getSecondOperand()].setValue(memory[inst.getFirstOperand()].getValue());
+        return true;
+    }
+}
+
+bool Simulation::funcCPYI(const Instruction &inst) {
+    if(inst.getFirstOperand() > memory.size() || inst.getSecondOperand() > memory.size())
+        return false;
+    else{
+        int tempAddress = memory[inst.getFirstOperand()].getValue();
+        memory[tempAddress].setValue(memory[inst.getSecondOperand()].getValue());
+        return true;
+    }
+}
+
+bool Simulation::funcCPYI2(const Instruction &inst) {
+    if(inst.getFirstOperand() > memory.size() || inst.getSecondOperand() > memory.size())
+
+        return false;
+    else{
+        int tempAddress = memory[inst.getSecondOperand()].getValue();
+        memory[tempAddress].setValue(memory[inst.getFirstOperand()].getValue());
+        return true;
+    }
+}
+
+bool Simulation::funcADD(const Instruction &inst) {
+    if(memory.size() < inst.getSecondOperand())
+        return false;
+    else{
+        memory[inst.getSecondOperand()].setValue(memory[inst.getSecondOperand()].getValue() + inst.getFirstOperand());
+        return true;
+    }
+}
+
+bool Simulation::funcADDI(const Instruction &inst) {
+    if(inst.getFirstOperand() > memory.size() || inst.getSecondOperand() > memory.size())
+        return false;
+    else{
+        memory[inst.getSecondOperand()].setValue(memory[inst.getFirstOperand()].getValue() + memory[inst.getSecondOperand()].getValue());
+        return true;
+    }
+}
+
+bool Simulation::funcSUBI(const Instruction &inst) {
+    if(inst.getFirstOperand() > memory.size() || inst.getSecondOperand() > memory.size())
+        return false;
+    else{
+        memory[inst.getSecondOperand()].setValue(memory[inst.getFirstOperand()].getValue() - memory[inst.getSecondOperand()].getValue());
+        return true;
+    }
+}
+
+bool Simulation::funcJIF(const Instruction &inst) {
+    if(inst.getFirstOperand() > memory.size())
+        return false;
+    else{
+        if(memory[inst.getFirstOperand()].getValue() <= 0){
+            memory[0].setValue(inst.getFirstOperand());
+        }
+        return true;
+    }
+}
+
+bool Simulation::funcHLT(const Instruction &inst) {
+    return true;
+}
+
+bool Simulation::funcSYS(const Instruction &inst) {
+    cerr << "This instruction is not implemented as it should not be(Reserved for next homework)" << endl;
+    return false;
 }
--- HW01/Simulation.h
+++ HW01/Simulation.h
@@ -12,7 +12,10 @@ using namespace std;
 
 #ifndef HW01_SIMULATION_H
 #define HW01_SIMULATION_H
-
+#define DATA_START "Begin Data Section"
+#define DATA_END "End Data Section"
+#define INSTR_START "Begin Instruction Section"
+#define INSTR_END "End Instruction Section"
 
 class Simulation {
 public:
@@ -20,9 +23,21 @@ public:
     ~Simulation();
     void parseFile(string &filename);
     void printMemory();
+    void printInstructionList();
     void cpuRun();
     Memory& getMemory(int index);
     Instruction& getInstruction(int index);
+    bool funcSET(const Instruction& inst);
+    bool funcCPY(const Instruction& inst);
+    bool funcCPYI(const Instruction& inst);
+    bool funcCPYI2(const Instruction& inst);
+    bool funcADD(const Instruction& inst);
+    bool funcADDI(const Instruction& inst);
+    bool funcSUBI(const Instruction& inst);
+    bool funcJIF(const Instruction& inst);
+    bool funcHLT(const Instruction& inst);
+    bool funcSYS(const Instruction& inst);
+
 private:
     vector<Memory> memory;
     vector<Instruction> instructions;
