9a750f27bb6f11aa4aa67eb8d4607d5f57199904
kasimsuzen <kasmszen@gmail.com>
2016-03-09 21:22:15 +0200

    getters and setters implemented for instruction class

--- HW01/Instruction.cpp
+++ HW01/Instruction.cpp
@@ -1,5 +1,5 @@
 //
-// Created by siyah on 07.03.2016.
+// Created by Kasım Süzen on 07.03.2016.
 //
 
 #include "Instruction.h"
@@ -13,4 +13,40 @@ Instruction::Instruction(int i, string &s, int a, int b) {
 
 Instruction::~Instruction() {
 
+}
+
+Instruction::Instruction() {
+
+}
+
+int Instruction::getIndex() const {
+    return index;
+}
+
+void Instruction::setIndex(int index) {
+    Instruction::index = index;
+}
+
+const string &Instruction::getInstruction() const {
+    return command;
+}
+
+void Instruction::setInstruction(const string &command) {
+    Instruction::command = command;
+}
+
+int Instruction::getFirstOperand() const {
+    return A;
+}
+
+void Instruction::setFirstOperand(int A) {
+    Instruction::A = A;
+}
+
+int Instruction::getSecondOperand() const {
+    return B;
+}
+
+void Instruction::setSecondOperand(int B) {
+    Instruction::B = B;
 }
