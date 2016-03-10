f6b85a01807b3d2faf9e19d61449e137f9950fe8
kasimsuzen <kasmszen@gmail.com>
2016-03-10 20:45:34 +0200

    Driver code updated and usage function implemented

--- HW01/main.cpp
+++ HW01/main.cpp
@@ -6,13 +6,24 @@
 #include "Memory.h"
 
 using namespace std;
+void usage();
 
-int main() {
+int main(int argc,char ** argv) {
+    if(argc < 3)
+        usage();
 
-    string filename("pr.txt");
+    string filename(argv[1]);
 
-    Simulation sim(filename, 2);
+    Simulation sim(filename, atoi(argv[3]));
     sim.cpuRun();
 
     return 0;
+}
+
+void usage(){
+    cout << "This program should run like this \n";
+    cout << "Simulate fileName -D mode" << endl;
+    cout << "Mode 0 runs all instructions and show last state of memory" << endl;
+    cout << "Mode 1 run one instruction and show memory map all instructions finished"<< endl;
+    cout << "Mode 2 runs one instruction shows memory map and wait to press a key " << endl;
 }
